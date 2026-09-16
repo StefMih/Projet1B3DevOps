2bc713a57c575dcf16247868786cd7f61aa5a1c2
Konstantinos <konstante47@gmail.com>
2026-09-16 15:57:04 +0200

    v2 mieux

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-
-const taches = [
+let taches = [
     "Acheter du pain",
-    "Comprendre les étapes DevOps",
-    "Apprendre Docker"
+    "Comprendre les étapes DevOps"
 ];
 
 const listeElement = document.getElementById("todo-list");
+const inputElement = document.getElementById("task-input");
+const btnAjouter = document.getElementById("add-btn");
 
 function afficherListe() {
     listeElement.innerHTML = ""; 
@@ -13,8 +13,23 @@ function afficherListe() {
     taches.forEach(tache => {
         const li = document.createElement("li");
         li.textContent = tache;
+        li.className = "task-card"; 
         listeElement.appendChild(li);
     });
 }
+
+
+function ajouterTache() {
+    const texte = inputElement.value.trim(); 
+
+    if (texte !== "") { 
+        taches.push(texte); 
+        afficherListe();    
+        inputElement.value = ""; 
+    }
+}
+
+btnAjouter.addEventListener("click", ajouterTache);
+
 
 afficherListe();
