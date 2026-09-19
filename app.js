4aafc2002e30c9571aa3b6aa972426bbabef00f4
Julian Tayeg <la248846@student.helha.be>
2026-09-19 15:31:53 +0200

    Ajout de la fonctionnalité de suppression de tâches

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 let taches = [
-    "Acheter du pain",
-    "Comprendre les étapes DevOps"
-];
+        "Acheter du pain",
+        "Comprendre les étapes DevOps"
+    ];
 
 const listeElement = document.getElementById("todo-list");
 const inputElement = document.getElementById("task-input");
 const btnAjouter = document.getElementById("add-btn");
+const clearBtn = document.getElementById("clear-btn");
 
 function afficherListe() {
     listeElement.innerHTML = ""; 
 
-    taches.forEach(tache => {
-        const li = document.createElement("li");
-        li.textContent = tache;
-        li.className = "task-card"; 
-        listeElement.appendChild(li);
+    taches.forEach((tache, index) => {
+    const li = document.createElement("li");
+    li.className = "task-card"; 
+
+    const textSpan = document.createElement("span");
+    textSpan.textContent = tache;
+
+    const deleteBtn = document.createElement("span");
+    deleteBtn.textContent = "✖";
+    deleteBtn.className = "delete-task-btn";
+
+    deleteBtn.addEventListener("click", () => {
+        supprimerTache(index);
     });
+
+    li.appendChild(textSpan);
+    li.appendChild(deleteBtn);
+    
+    listeElement.appendChild(li);
+});
+
 }
 
 
@@ -25,11 +41,31 @@ function ajouterTache() {
     if (texte !== "") { 
         taches.push(texte); 
         afficherListe();    
-        inputElement.value = ""; 
+        inputElement.value = "";
     }
 }
 
+function supprimerEntrée() {
+    inputElement.value = ""; 
+    
+}
+
+function supprimerTache(index) {
+    taches.splice(index, 1);
+    afficherListe();
+}
+
+
+
+clearBtn.addEventListener("click", supprimerEntrée);
+
 btnAjouter.addEventListener("click", ajouterTache);
+
+inputElement.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        ajouterTache();
+    }
+});
 
 
 afficherListe();
